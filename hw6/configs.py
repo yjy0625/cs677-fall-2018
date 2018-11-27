40a64ea7a5f560d0dd28437e0e0040137832d90b
Jingyun Yang <jingyuny@usc.edu>
2018-11-27 03:11:37 -0800

    Update configs file.

--- hw6/configs.py
+++ hw6/configs.py
@@ -9,14 +9,14 @@ flags.DEFINE_string("prefix", "default", "Nickname for the experiment [default]"
 flags.DEFINE_boolean("use_bilinear_deconv2d", False, "Whether to use bilinear deconv2d [False]")
 
 # training params
-flags.DEFINE_integer("max_steps", 100000, "Number of steps to train. [5000]")
+flags.DEFINE_integer("max_steps", 50000, "Number of steps to train. [50000]")
 flags.DEFINE_float("learning_rate", 1e-3, "Learning rate for the model. [1e-3]")
 flags.DEFINE_float("momentum", 0.99, "Momentum for the optimizer. [0.99]")
 flags.DEFINE_integer("batch_size", 1, "Number of images in batch [1]")
 
 # logging params
-flags.DEFINE_integer("log_step", 100, "Interval for console logging [10]")
-flags.DEFINE_integer("val_step", 100, "Interval for validation [50]")
+flags.DEFINE_integer("log_step", 100, "Interval for console logging [100]")
+flags.DEFINE_integer("val_step", 100, "Interval for validation [100]")
 flags.DEFINE_integer("save_checkpoint_step", 500, "Interval for checkpoint saving [500]")
 
 # checkpoint for testing
