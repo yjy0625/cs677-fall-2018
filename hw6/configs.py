171ec317208f77f55e674475964eff9f341ecac3
Jingyun Yang <jingyuny@usc.edu>
2018-11-26 20:27:03 -0800

    Fixed testing code.

--- hw6/configs.py
+++ hw6/configs.py
@@ -19,6 +19,9 @@ flags.DEFINE_integer("log_step", 100, "Interval for console logging [10]")
 flags.DEFINE_integer("val_step", 100, "Interval for validation [50]")
 flags.DEFINE_integer("save_checkpoint_step", 500, "Interval for checkpoint saving [500]")
 
+# checkpoint for testing
+flags.DEFINE_string("checkpoint", None, "Checkpoint path for testing [None]")
+
 # gpu
 flags.DEFINE_integer("gpu", 0, "GPU to use [0]")
 
--- hw6/test.py
+++ hw6/test.py
@@ -10,85 +10,85 @@ from configs import config
 from main import run_single_step
 
 def main():
-    # limit GPU's visible by the program
-    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-    os.environ["CUDA_VISIBLE_DEVICES"] = str(config.gpu)
-    print("Using GPU {}.".format(config.gpu))
+	# limit GPU's visible by the program
+	os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+	os.environ["CUDA_VISIBLE_DEVICES"] = str(config.gpu)
+	print("Using GPU {}.".format(config.gpu))
 
-    # load data
-    print("Loading datasets")
-    _, _, dataset_test = load_datasets('data')
-    config.data_info = list(dataset_train['image'].shape[1:])
-    print("Dataset loading completes.")
+	# load data
+	print("Loading datasets")
+	_, _, dataset_test = load_datasets('data')
+	config.data_info = list(dataset_test['image'].shape[1:])
+	print("Dataset loading completes.")
 
-    tf.reset_default_graph()
+	tf.reset_default_graph()
 
-    model = Model(config)
-    
-    session_config = tf.ConfigProto(
-        gpu_options=tf.GPUOptions(allow_growth=True),
-        device_count{'GPU': 1}
-    )
+	model = Model(config)
+	
+	session_config = tf.ConfigProto(
+		gpu_options=tf.GPUOptions(allow_growth=True),
+		device_count={'GPU': 1}
+	)
 
-    all_vars = tf.trainable_variables()
+	all_vars = tf.trainable_variables()
 
-	ckpt_dir = config.checkpoint
-    summary_writer = tf.summary.FileWriter(train_dir)
+	ckpt_path = config.checkpoint
+	ckpt_dir = osp.dirname(ckpt_path)
+	summary_writer = tf.summary.FileWriter(ckpt_dir)
 
-    with tf.Session(config=session_config) as sess:
-        # restore checkpoint
-        if ckpt_dir is not None:
-            print("Checkpoint path: %s", self.ckpt_path)
-            tf.train.Saver(var_list=all_vars).restore(
-                sess, tf.train.last_checkpoint(ckpt_dir))
-            print("Loaded the parameters from the provided checkpoint path")
-        else:
-            print("Please provide checkpoint path.")
-            exit(0)
+	with tf.Session(config=session_config) as sess:
+		sess.run(tf.global_variables_initializer())
+		sess.run(tf.local_variables_initializer())
 
-        test_logfile = os.path.join(ckpt_dir, 'test_result.txt')
-        n_test = dataset_test['image'].shape[0]
-        ind_exp_results = {
-            "loss": [],
-            "tp": [],
-            "fp": [],
-            "fn": [],
-            "pixel_level_iou": [],
-            "pred": [],
-            "probs": []
-        }
-        for i in range(n_test):
-            test_batch = {key: dataset_test[key][[i]] for key in dataset_test}
-            exp_results = run_single_step(sess, model, test_batch, 
-                                          summary_writer, 
-                                          mode='test',
-                                          test_logfile=test_logfile)
-            for key in exp_results:
-                ind_exp_results[key].append(exp_results[key])
-        exp_results = {}
-        for key in ind_exp_results:
-            exp_results[key] = np.stack(ind_exp_results[key])
-        exp_results["loss"] = np.mean(exp_results["loss"])
-        exp_results["tp"] = np.sum(exp_results["tp"])
-        exp_results["fp"] = np.sum(exp_results["fp"])
-        exp_results["fn"] = np.sum(exp_results["fn"])
-        exp_results["pixel_level_iou"] = exp_results["tp"] / (
-                exp_results["tp"] + exp_results["fp"] + exp_results["fn"])
-        print("Test Average Loss: {:3f}; Test IOU: {:3f}".format(
-                exp_results["loss"], exp_results["pixel_level_iou"]))
+		# restore checkpoint
+		if ckpt_dir is not None:
+			print("Checkpoint path: %s", ckpt_path)
+			tf.train.Saver(var_list=all_vars).restore(
+				sess, ckpt_path)
+			print("Loaded the parameters from the provided checkpoint path")
+		else:
+			print("Please provide checkpoint path.")
+			exit(0)
 
-        # add loss curves to experiment results
-        exp_results['train_losses'] = train_losses
-        exp_results['val_losses'] = val_losses
+		test_logfile = os.path.join(ckpt_dir, 'test_result.txt')
+		n_test = dataset_test['image'].shape[0]
+		ind_exp_results = {
+			"loss": [],
+			"tp": [],
+			"fp": [],
+			"fn": [],
+			"pixel_level_iou": [],
+			"pred": [],
+			"probs": []
+		}
+		for i in range(n_test):
+			test_batch = {key: dataset_test[key][[i]] for key in dataset_test}
+			exp_results = run_single_step(sess, model, test_batch, 
+										  summary_writer, 
+										  mode='test',
+										  test_logfile=test_logfile)
+			for key in exp_results:
+				ind_exp_results[key].append(exp_results[key])
+		exp_results = {}
+		for key in ind_exp_results:
+			exp_results[key] = np.stack(ind_exp_results[key])
+		exp_results["loss"] = np.mean(exp_results["loss"])
+		exp_results["tp"] = np.sum(exp_results["tp"])
+		exp_results["fp"] = np.sum(exp_results["fp"])
+		exp_results["fn"] = np.sum(exp_results["fn"])
+		exp_results["pixel_level_iou"] = exp_results["tp"] / (
+				exp_results["tp"] + exp_results["fp"] + exp_results["fn"])
+		print("Test Average Loss: {:3f}; Test IOU: {:3f}".format(
+				exp_results["loss"], exp_results["pixel_level_iou"]))
 
-        # log test results
-        with open(os.path.join(ckpt_dir, 'test_result.p'), 'wb') as f:
-            pickle.dump(exp_results, f)
-            print("Logged experiment results to {}".format(f.name))
+		# log test results
+		with open(os.path.join(ckpt_dir, 'test_result.p'), 'wb') as f:
+			pickle.dump(exp_results, f)
+			print("Logged experiment results to {}".format(f.name))
 
-        # flush Tensorboard summaries
-        summary_writer.flush()
+		# flush Tensorboard summaries
+		summary_writer.flush()
 
-        
+		
 if __name__ == '__main__':
-    main()
+	main()
