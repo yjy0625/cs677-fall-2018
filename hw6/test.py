96ba1400e21ed7f8feea5214ac06599455275aea
Jingyun Yang <jingyuny@usc.edu>
2018-11-26 20:02:17 -0800

    Completed testing file.

--- hw6/test.py
+++ hw6/test.py
@@ -7,6 +7,7 @@ import pickle
 from data_util import *
 from model import Model
 from configs import config
+from main import run_single_step
 
 def main():
     # limit GPU's visible by the program
@@ -31,16 +32,63 @@ def main():
 
     all_vars = tf.trainable_variables()
 
-    self.pretrain_saver = 
-	self.ckpt_path = config.checkpoint
-    if self.ckpt_path is not None:
-        print("Checkpoint path: %s", self.ckpt_path)
-		tf.train.Saver(var_list=all_vars, max_to_keep=1).restore(
-			self.session, self.ckpt_path)
-        print("Loaded the parameters from the provided checkpoint path")
-	else:
-		print("Please provide checkpoint path.")
-		exit(0)
+	ckpt_dir = config.checkpoint
+    summary_writer = tf.summary.FileWriter(train_dir)
 
     with tf.Session(config=session_config) as sess:
+        # restore checkpoint
+        if ckpt_dir is not None:
+            print("Checkpoint path: %s", self.ckpt_path)
+            tf.train.Saver(var_list=all_vars).restore(
+                sess, tf.train.last_checkpoint(ckpt_dir))
+            print("Loaded the parameters from the provided checkpoint path")
+        else:
+            print("Please provide checkpoint path.")
+            exit(0)
+
+        test_logfile = os.path.join(ckpt_dir, 'test_result.txt')
+        n_test = dataset_test['image'].shape[0]
+        ind_exp_results = {
+            "loss": [],
+            "tp": [],
+            "fp": [],
+            "fn": [],
+            "pixel_level_iou": [],
+            "pred": [],
+            "probs": []
+        }
+        for i in range(n_test):
+            test_batch = {key: dataset_test[key][[i]] for key in dataset_test}
+            exp_results = run_single_step(sess, model, test_batch, 
+                                          summary_writer, 
+                                          mode='test',
+                                          test_logfile=test_logfile)
+            for key in exp_results:
+                ind_exp_results[key].append(exp_results[key])
+        exp_results = {}
+        for key in ind_exp_results:
+            exp_results[key] = np.stack(ind_exp_results[key])
+        exp_results["loss"] = np.mean(exp_results["loss"])
+        exp_results["tp"] = np.sum(exp_results["tp"])
+        exp_results["fp"] = np.sum(exp_results["fp"])
+        exp_results["fn"] = np.sum(exp_results["fn"])
+        exp_results["pixel_level_iou"] = exp_results["tp"] / (
+                exp_results["tp"] + exp_results["fp"] + exp_results["fn"])
+        print("Test Average Loss: {:3f}; Test IOU: {:3f}".format(
+                exp_results["loss"], exp_results["pixel_level_iou"]))
+
+        # add loss curves to experiment results
+        exp_results['train_losses'] = train_losses
+        exp_results['val_losses'] = val_losses
+
+        # log test results
+        with open(os.path.join(ckpt_dir, 'test_result.p'), 'wb') as f:
+            pickle.dump(exp_results, f)
+            print("Logged experiment results to {}".format(f.name))
+
+        # flush Tensorboard summaries
+        summary_writer.flush()
+
         
+if __name__ == '__main__':
+    main()
